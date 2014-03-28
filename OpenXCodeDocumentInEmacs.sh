b97b68ff6741becc887c90ac982eb6bc3b5b5a6c
David Hart <davidhart17@gmail.com>
2014-03-28 14:39:01 -0700

    Fix script for when document is edited in XCode

--- OpenXCodeDocumentInEmacs.sh
+++ OpenXCodeDocumentInEmacs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 osascript -e 'tell application "Xcode"
-	set currentActiveDocument to document 1 whose name ends with (word -1 of (get name of window 1))
+	set windowName to get name of window 1
+	set windowNameWordCount to count of words of windowName
+	set currentActiveDocumentName to (word -1 of windowName)
+	if (currentActiveDocumentName is equal to "Edited")
+    	set currentActiveDocumentName to (word -2 of windowName)
+	end if
+--    display alert windowNameWordCount
+--    display alert (word -1 of windowName)
+	set currentActiveDocument to document 1 whose name ends with currentActiveDocumentName
 	set currentActiveDocumentPath to path of currentActiveDocument
 --	set	currentInsertionPoint to selected character range of currentActiveDocument
 --	set output to ("Song: " & song & " - " & artist & " - " & album)
